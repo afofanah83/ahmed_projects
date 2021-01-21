bac72697c789eecae27d723a4e06a3ee353a8ec7
Ahmed Fofanah <ahmedfofanah@Ahmeds-MacBook-Pro.local>
2021-01-21 13:05:31 -0500

    two new files added

---
+++ magicnum.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	x = 100000;
+	
+	int y = x / 10; 	
+
+	printf("The magic number is: %i\nGod Bless You.\n", y);
+	return 0;
+}
+
+
+//Expression - Anything that can be evaluated (to 1 value)
+//statement
+//declaration
+//initialization
+
+//c programming is space insensitive language.
